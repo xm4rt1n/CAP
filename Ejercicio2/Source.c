2d066e5acd4aa9c60dbda50d194d6d4aa170ab45
xm4rt1n <adrianmartin2505@gmail.com>
2023-02-07 00:06:04 +0100

    comentarios actualizados

--- Ejercicio2/Source.c
+++ Ejercicio2/Source.c
@@ -27,25 +27,36 @@ void main() {
 		*(array1 + i) = rand();
 	}
 
-	for (int i = 0; i < n; i++) {
-		printf("%d,", *(array1 + i));
+	// si el numero de elementos es pequeño imprimimos el vector. Usado para ver que funciona correctametne el algoritmo
+	if (n < 10) {
+		for (int i = 0; i < n; i++) {
+			printf("%d,", *(array1 + i));
+		}
 	}
+	
 	printf("\n");
+
+	// obtenemos el tiempo en el que se inicia la ejecucion del algoritmo y lo guardamos en una variable
 	double t0 = omp_get_wtime();
 
+	// llamamos al algoritmo quicksort 
 	quicksort(array1, n, 5000);
 
+	//obtenemos el tiempo en el que acaba la ejecucion del algoritmo.
 	double t1 = omp_get_wtime();
 
 	printf("\n");
 
+	// imprimimos el array una vez se ha ordenado. 
 	for (int i = 0; i < n; i++) {
 		printf("%d, ", *(array1 + i));
 	}
 	printf("\n");
 
+	// imprimimos el tiempo que ha tardado el algoritmo restando el final menos inicio.
 	printf("El tiempo es: %f", t1 - t0);
 
+	//liberamos la memoria dinamica.
 	free(array1);
 
 }
@@ -57,10 +68,10 @@ void quicksort(int* array, int nElem, int pivote) {
 	int igualesPivote = 0;
 	int* copiaArray;
 
-	if (nElem <= 1) {
+	if (nElem <= 1) { //si el numero de elementos es 0 o 1 devolvemos el array tal cual ya que no hay nada que ordenar.
 		return;
 	}
-	if (nElem == 2) {
+	if (nElem == 2) { //si el nº de elementos es dos, comprobamos si estan correctamente ordenados, de no ser asi los intercambiamos
 		if (array[0] > array[1]) {
 			int aux;
 			aux = array[0];
@@ -69,20 +80,21 @@ void quicksort(int* array, int nElem, int pivote) {
 		}
 		return;
 	}
-
+	// si el numero de elementos es >2 creamos un array de ayuda en memoria dinamica
 	copiaArray = (int*)malloc(nElem * sizeof(int));
 	if (copiaArray == NULL) {
 		printf("No se pudo reservar la memoria dinamica");
 		exit;
 	}
 
+	// tras ello recorremos el array original y vamos anotando si es mayor, menos o igual que el pivote
 	for (int i = 0; i < nElem; i++) {
 		if (array[i] < pivote) {
-			copiaArray[menoresPivote] = array[i];
+			copiaArray[menoresPivote] = array[i]; // los valores menores que el pivote los colocamos en el inicio del array de apoyo
 			menoresPivote++;
 		}
 		if (array[i] > pivote) {
-			copiaArray[nElem-mayoresPivote-1] = array[i];
+			copiaArray[nElem-mayoresPivote-1] = array[i]; //los valores mayores que el pivote los colocamos desde el final del array de apoyo
 			mayoresPivote++;
 		}
 		if (array[i] == pivote) {
@@ -91,12 +103,13 @@ void quicksort(int* array, int nElem, int pivote) {
 	}
 
 	for (int i = 0; i < igualesPivote; i++) {
-		copiaArray[menoresPivote + i] = pivote;
+		copiaArray[menoresPivote + i] = pivote; //acabamos de rellenar el array de apoyo con los valores iguales que el pivote
 	}
 
-	memcpy(array, copiaArray, nElem * sizeof(int));
+	memcpy(array, copiaArray, nElem * sizeof(int)); //copiamos el valores del array de apoyo al array original y liberamos la memoria dinamica
 	free(copiaArray);
 
+	// realizamos llamadas recursivas dividiendo cada vez nuestro vector en funcion de si son mayores que el pivote o menores
 	quicksort(array, menoresPivote, pivote / 2);
 	int posMayores = nElem - mayoresPivote;
 	quicksort(array + posMayores, mayoresPivote, pivote * 3 / 2);
